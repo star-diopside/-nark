9ead0a62cc440c23ef7c7cadeee903e903b3ec77
se-yoshi <se.yoshi@gmail.com>
2016-10-02 21:33:33 +0900

    war作成プロジェクトを追加

--- nark-web/src/main/java/jp/gr/java_conf/star_diopside/nark/Server.java
+++ nark-web/src/main/java/jp/gr/java_conf/star_diopside/nark/Server.java
@@ -2,11 +2,18 @@ package jp.gr.java_conf.star_diopside.nark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Server {
+public class Server extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Server.class);
     }
 }
