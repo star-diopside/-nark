156303d202d965c43b5893620dfba736e6039cb4
se-yoshi <se.yoshi@gmail.com>
2016-10-10 03:25:24 +0900

    Spring Sessionの適用

---
+++ nark-web/src/main/java/jp/gr/java_conf/star_diopside/nark/config/HttpSessionConfig.java
@@ -0,0 +1,7 @@
+package jp.gr.java_conf.star_diopside.nark.config;
+
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@EnableJdbcHttpSession
+public class HttpSessionConfig {
+}
